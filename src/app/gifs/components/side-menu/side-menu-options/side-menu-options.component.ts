e8b36efe96175c0f7fc9651b1c2484604d8241f3
Jorge Torres Romero <jtorresromero30@gmail.com>
2025-03-10 18:41:17 -0600

    10-03-2025(3)

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Injectable } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { GifService } from 'src/app/gifs/services/gifs.service';
+
+
+
+interface MenuOption{
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'app-side-menu-options',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './side-menu-options.component.html',
   // styleUrl: './side-menu-options.component.css'
 })
 export class SideMenuOptionsComponent {
+  gifService = inject (GifService)
 
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populars',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Searching',
+      subLabel: 'Search Gifs',
+      route: '/dashboard/search'
+    },
+  ]
 }
+
+
